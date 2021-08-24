c192c73f190796feb7d2c3ff521a3c2d86d4e1a9
hemory <hphife@yahoo.com>
2021-08-23 20:50:40 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Hemory";
+            string lastName = "Phifer";
+
+            Console.WriteLine(firstName + lastName);
+
+        }
+    }
+}
